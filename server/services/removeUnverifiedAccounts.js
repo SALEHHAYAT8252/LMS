271a0474c4bc670be1f2f7ae34754747ef8feb0f
Saleh Hayat <salehhayat515@gmail.com>
2025-05-02 13:54:33 +0530

    change time run in each 10 minute in removeUnverifiedAccount

--- server/services/removeUnverifiedAccounts.js
+++ server/services/removeUnverifiedAccounts.js
@@ -2,12 +2,18 @@ import cron from 'node-cron';
 import { User } from '../models/userModel.js';
 
 export const removeUnverifiedAccounts = () => {
-    cron.schedule("* * * * * ", async () => {
+    cron.schedule("*/10 * * * * ", async () => {
         const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000); // 30 minutes ago
         await User.deleteMany({
             isVerified: false,
             createdAt: { $lt: thirtyMinutesAgo },
         });
-        console.log("Unverified accounts older than 30 minutes have been removed.");    
+
+        let count =1;
+        if(count > 0) {
+        console.log("Unverified accounts older than 30 minutes have been removed.");
+        count--;
+        } 
+
     })
 }
